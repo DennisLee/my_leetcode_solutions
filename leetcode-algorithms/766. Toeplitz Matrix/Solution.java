95589fa767bd245a3f4c3a3cf3bf67672c2ffc63
Zhe Li <zheli@Zhes-MBP.hsd1.ca.comcast.net>
2018-06-06 15:58:49 -0700

    ..

--- leetcode-algorithms/766. Toeplitz Matrix/Solution.java
+++ leetcode-algorithms/766. Toeplitz Matrix/Solution.java
@@ -2,7 +2,6 @@
  * Time: O(n)
  * Space: O(1)
  */
-
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
         int matrixRow = matrix.length;
@@ -34,6 +33,25 @@ class Solution {
             }
         }
         
+        return true;
+    }
+}
+
+/**
+ * Compare with the top-left element
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int r = 0; r < matrix.length; r++){
+            for (int c = 0; c < matrix[0].length; c++){
+                if (r > 0 && c > 0 && matrix[r-1][c-1] != matrix[r][c]){
+                    return false;
+                }
+            }
+        }
+         
         return true;
     }
 }
